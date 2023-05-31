b1d95dfb48c5963d4d2331c22c77aa105f572c35
Sven <sven.giegerich@mailbox.org>
2023-05-31 12:34:30 +0200

    Apply suggestions from code review

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,9 @@
-set "PKG_NAME=escape_utils"
+set "PKG_NAME=rb-escape_utils"
 if %errorlevel% NEQ 0 exit /b %errorlevel%
 
 gem install -N -l -V --norc --ignore-dependencies %PKG_NAME%-%PKG_VERSION%.gem
 if %errorlevel% NEQ 0 exit /b %errorlevel%
 gem unpack %PKG_NAME%-%PKG_VERSION%.gem
 if %errorlevel% NEQ 0 exit /b %errorlevel%
-cp %PKG_NAME%-*/LICENSE .
+cp %PKG_NAME%/LICENSE .
 if %errorlevel% NEQ 0 exit /b %errorlevel%
